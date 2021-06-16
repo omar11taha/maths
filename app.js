8b252129a66a04623d94269a52a1b69cd750c94d
OmarTaha4 <>
2021-06-16 12:52:10 +0000

    new file

---
+++ app.js
@@ -0,0 +1,17 @@
+alert("math")
+var name = prompt("2+5=")
+if(name == 7){
+  alert ("great ")
+}
+else{
+   alert('try again')
+}
+
+var x = prompt("7*7")
+
+if(x == 49){
+  alert ("great ")
+}
+else{
+   alert('try again')
+}
