72c2d5b3f0651771626512bcbd7d4b807d8119d4
OmarTaha4 <>
2021-06-17 13:07:28 +0000

    new css

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 alert("math")
 var name = prompt("2+5=")
+while(name != "7"){
+  name = prompt("please,try agane!(2+5)")
+}
+
+
 if(name == 7){
   alert ("great ")
 }
@@ -7,11 +12,36 @@ else{
    alert('try again')
 }
 
-var x = prompt("7*7")
+var mal = prompt("7*7")
+  
 
-if(x == 49){
+if( mal == 49){
   alert ("great ")
+  
+}
+ else {
+   alert('try again')
+   for(y = 1 ; y<=3;y++){
+  prompt('you have #three trials#(7*7=) ')}
+}
+
+var  math = prompt('calculation-method')
+
+
+if (math == 'Multiplication' ){
+ 
+ document.write('<h3> multiply</h3>')
+
+   document.write( '<img src="https://thirdspacelearning.com/wp-content/uploads/2020/05/long-multiplication-method-how-to-teach-1024x538.jpg" />' )
+  
 }
 else{
-   alert('try again')
+  alert('try again')
+}
+var imgR = prompt('HWO many times you need to see the photo')
+
+for ( i =0 ; i<= imgR;i++){
+  
+   document.write( '<img src="https://thirdspacelearning.com/wp-content/uploads/2020/05/long-multiplication-method-how-to-teach-1024x538.jpg" />' )
+
 }
